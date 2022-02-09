8734b0dc77dbf781a9f4a0cbb75429f5900113e5
RAHUL ROY <rahulkumarvivoy51l@gmail.com>
2022-02-10 00:55:40 +0530

    test assignment

---
+++ src/App.tsx
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+import { TezosToolkit } from "@taquito/taquito";
+import "./App.css";
+import ConnectButton from "./components/ConnectWallet";
+import DisconnectButton from "./components/DisconnectWallet";
+import qrcode from "qrcode-generator";
+
+
+enum BeaconConnection {
+  NONE = "",
+  LISTENING = "Listening to P2P channel",
+  CONNECTED = "Channel connected",
+  PERMISSION_REQUEST_SENT = "Permission request sent, waiting for response",
+  PERMISSION_REQUEST_SUCCESS = "Wallet is connected"
+}
+
+const App = () => {
+  const [Tezos, setTezos] = useState<TezosToolkit>(
+    new TezosToolkit("https://hangzhounet.api.tez.ie")
+  );
+  const [contract, setContract] = useState<any>(undefined);
+  const [publicToken, setPublicToken] = useState<string | null>("");
+  const [wallet, setWallet] = useState<any>(null);
+  const [userAddress, setUserAddress] = useState<string>("");
+  const [userBalance, setUserBalance] = useState<number>(0);
+  const [storage, setStorage] = useState<number>(0);
+  const [copiedPublicToken, setCopiedPublicToken] = useState<boolean>(false);
+  const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState<string>("transfer");
+  const contractAddress: string = "KT1WiPWNcBMcXJButkkvroRGkzs45n3iZ13c";
+
+  const generateQrCode = (): { __html: string } => {
+    const qr = qrcode(0, "L");
+    qr.addData(publicToken || "");
+    qr.make();
+
+    return { __html: qr.createImgTag(4) };
+  };
+
+  if (publicToken && (!userAddress || isNaN(userBalance))) {
+    return (
+      <div className="main-box">
+        <h1>Test Assignment</h1>
+        <div id="dialog">
+          <header>Try the Taquito Boilerplate App!</header>
+          <div id="content">
+            <p className="text-align-center">
+              <i className="fas fa-broadcast-tower"></i>&nbsp; Connecting to
+              your wallet
+            </p>
+            <div
+              dangerouslySetInnerHTML={generateQrCode()}
+              className="text-align-center"
+            ></div>
+            <p id="public-token">
+              {copiedPublicToken ? (
+                <span id="public-token-copy__copied">
+                  <i className="far fa-thumbs-up"></i>
+                </span>
+              ) : (
+                <span
+                  id="public-token-copy"
+                  onClick={() => {
+                    if (publicToken) {
+                      navigator.clipboard.writeText(publicToken);
+                      setCopiedPublicToken(true);
+                      setTimeout(() => setCopiedPublicToken(false), 2000);
+                    }
+                  }}
+                >
+                  <i className="far fa-copy"></i>
+                </span>
+              )}
+
+              <span>
+                Public token: <span>{publicToken}</span>
+              </span>
+            </p>
+            <p className="text-align-center">
+              Status: {beaconConnection ? "Connected" : "Disconnected"}
+            </p>
+          </div>
+        </div>
+        <div id="footer">
+          <img src="built-with-taquito.png" alt="Built with Taquito" />
+        </div>
+      </div>
+    );
+  } else if (userAddress && !isNaN(userBalance)) {
+    return (
+      <div className="main-box">
+        <h1>Test Assignment</h1>
+        <div id="tabs">
+          <div
+            id="transfer"
+            className={activeTab === "transfer" ? "active" : ""}
+            onClick={() => setActiveTab("transfer")}
+          >
+            Account Detail
+          </div>
+        </div>
+        <div id="dialog">
+          <div id="content">
+            {
+              activeTab === "transfer" &&(
+                  <>
+                    <div id="transfers">
+                      <h3 className="text-align-center">Assignment to get balance</h3>
+                    </div>
+                  </>
+              )
+            }
+            <p>
+              <i className="far fa-file-code"></i>&nbsp;
+              <a
+                href={`https://better-call.dev/hangzhounet/${contractAddress}/operations`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {contractAddress}
+              </a>
+            </p>
+            <p>
+              <i className="far fa-address-card"></i>&nbsp; {userAddress}
+            </p>
+            <p className="account">
+              <i className="fas fa-piggy-bank"></i>&nbsp;
+              {(userBalance / 1000000).toLocaleString("en-US")} ꜩ
+            </p>
+          </div>
+          <DisconnectButton
+            wallet={wallet}
+            setPublicToken={setPublicToken}
+            setUserAddress={setUserAddress}
+            setUserBalance={setUserBalance}
+            setWallet={setWallet}
+            setTezos={setTezos}
+            setBeaconConnection={setBeaconConnection}
+          />
+        </div>
+        <div id="footer">
+          <img src="built-with-taquito.png" alt="Built with Taquito" />
+        </div>
+      </div>
+    );
+  } else if (!publicToken && !userAddress && !userBalance) {
+    return (
+      <div className="main-box">
+        <div className="title">
+          <h1>Test Assignment</h1>
+        </div>
+        <div id="dialog">
+          <header>Welcome to test assignment</header>
+          <div id="content">
+            <p>Hello!</p>
+            <p>
+              Check for a non-zero balance of a specific token
+              Display “Has Token” if user has token or “Does not have Token” if user does not have the token.
+            </p>
+          </div>
+          <ConnectButton
+            Tezos={Tezos}
+            setContract={setContract}
+            setPublicToken={setPublicToken}
+            setWallet={setWallet}
+            setUserAddress={setUserAddress}
+            setUserBalance={setUserBalance}
+            setStorage={setStorage}
+            contractAddress={contractAddress}
+            setBeaconConnection={setBeaconConnection}
+            wallet={wallet}
+          />
+        </div>
+        <div id="footer">
+          <img src="built-with-taquito.png" alt="Built with Taquito" />
+        </div>
+      </div>
+    );
+  } else {
+    return <div>An error has occurred</div>;
+  }
+};
+
+export default App;
